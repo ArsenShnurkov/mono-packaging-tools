feba7b79d0757e069227703be0e7d6151ef488a9
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-09-07 23:27:19 +0300

    v4

---
+++ mpt-gitmodules/Parser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Eto.Parse;
+using Eto.Parse.Grammars;
+using System.Diagnostics;
+
+namespace mptgitmodules
+{
+	public class Parser
+	{
+		Grammar myGrammar;
+		public Parser(string textGrammar, string root_rule)
+		{
+			var ebnfGrammar = new Eto.Parse.Grammars.EbnfGrammar( (EbnfStyle)
+				( EbnfStyle.Iso14977 
+			      - EbnfStyle.WhitespaceSeparator
+					+ EbnfStyle.EscapeTerminalStrings )
+			);
+			myGrammar = ebnfGrammar.Build(textGrammar, root_rule); 
+		}
+
+		public void DoProcessing(string textToParse)
+		{
+			var ast = myGrammar.Match (textToParse);
+			var m = ast.Matches ["variable_value", true].Matches; // for "evaluate" window of debugger
+			if (ast.Success == false) {
+				Trace.WriteLine (ast.ErrorMessage);
+				Console.ReadLine ();
+			}
+		}
+	}
+}
+
--- mpt-gitmodules/Program.cs
+++ mpt-gitmodules/Program.cs
@@ -4,36 +4,34 @@ using Eto.Parse.Grammars;
 using Eto.Parse.Writers;
 using Eto.Parse;
 using System.Text;
+using System.IO;
 
 namespace mptgitmodules
 {
-	class MainClass
+	class Globals
 	{
-		readonly static string myEbnfString = @"
-file_content := { file_unit } ;
-
-file_unit := eol | myws | nbody ;
-
-eol := ? Terminals.Eol ? ;
-
-nbody := { 'A' } ;
-
-myws := ' ' ;
-";
-		readonly static string fileContent = "\nA AA\n AAAA AA A \n";
-
 		public static void Main (string[] args)
 		{
-			var ebnfGrammar = new Eto.Parse.Grammars.EbnfGrammar(
-				EbnfStyle.Iso14977);
-			var myGrammar = ebnfGrammar.Build(myEbnfString, "file_content"); 
+			var grammar = "/var/calculate/remote/distfiles/egit-src/mono-packaging-tools.git/mpt-gitmodules/bnf2/syntax4.ebnf";
+			var textGrammar = LoadFile (grammar);
+			var root_rule = "file_content";
+			var Parser = new Parser (textGrammar, root_rule);
 
-			var ast = myGrammar.Match (fileContent);
-			if (ast.Success == false) {
-				Trace.WriteLine (ast.ErrorMessage);
-				Console.ReadLine ();
+			var content = "/var/calculate/remote/distfiles/egit-src/monodevelop.git/.gitmodules";
+			var textContent = LoadFile (content);
+			Parser.DoProcessing (textContent);
+		}
+
+
+		public static string LoadFile(string filename)
+		{
+			string fileContent = String.Empty;
+			using (var s = new FileStream (filename, FileMode.Open, FileAccess.Read)) {
+				using (var sr = new StreamReader (s)) {
+					fileContent = sr.ReadToEnd ();
+					return fileContent;
+				}
 			}
-			var m = ast.Matches ["eol", true].Matches; // for "evaluate" window of debugger
 		}
 	}
 }
