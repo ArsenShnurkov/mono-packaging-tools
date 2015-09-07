219b4e1c586a5e6666e421645492324a05746232
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2015-09-07 11:02:49 +0300

    WIP - nonworking parser

--- mpt-gitmodules/Parser.cs
+++ mpt-gitmodules/Parser.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using Eto.Parse.Writers;
 
 namespace mptgitmodules
 {
@@ -14,7 +14,7 @@ namespace mptgitmodules
 		Grammar grammar;
 		public Parser()
 		{
-			grammar = LoadGrammar ("/var/calculate/remote/distfiles/egit-src/mono-packaging-tools.git/mpt-gitmodules/bnf/syntax1.ebnf");
+			grammar = LoadGrammar ("/var/calculate/remote/distfiles/egit-src/mono-packaging-tools.git/mpt-gitmodules/bnf2/syntax3.ebnf");
 		}
 
 		string LoadFile(string filename)
@@ -31,21 +31,23 @@ namespace mptgitmodules
 		Grammar LoadGrammar(string filename)
 		{
 			string myEbnfString = LoadFile(filename);
-			var ebnfGrammar = new Eto.Parse.Grammars.EbnfGrammar(EbnfStyle.Iso14977);
-			var myGrammar = ebnfGrammar.Build(myEbnfString, "filecontent"); 
+			var ebnfGrammar = new Eto.Parse.Grammars.EbnfGrammar(
+				(EbnfStyle)(EbnfStyle.Iso14977 - EbnfStyle.WhitespaceSeparator));
+			var myGrammar = ebnfGrammar.Build(myEbnfString, "file_content"); 
 			return myGrammar;
 		}
 			
-		public string Parse (string filename, string SectionType, string SectionName)
+		void Dump(Match ast, StringBuilder res)
 		{
-			var fileContent = LoadFile (filename);
-			var res = new StringBuilder (fileContent.Length);
-			var ast = grammar.Match (fileContent);
-			if (ast.Success == false) {
-				Trace.WriteLine (ast.ErrorMessage);
-				Console.ReadLine ();
-				return String.Empty;
+			Trace.WriteLine(new DisplayParserWriter ().Write (ast.Parser));
+			foreach (var subnode in ast.Matches)
+			{
+				res.Append (subnode.StringValue);
 			}
+		}
+
+		void Cut(Match ast, StringBuilder res, string SectionType, string SectionName)
+		{
 			foreach (var subnode in ast.Matches)
 			{
 				if (IsSubnodeToRemove (subnode, SectionType, SectionName) == false)
@@ -53,8 +55,29 @@ namespace mptgitmodules
 					res.Append (subnode.StringValue);
 				}
 			}
+		}
+
+		public string Parse (string filename, string SectionType, string SectionName)
+		{
+			var fileContent = LoadFile (filename);
+			var res = new StringBuilder (fileContent.Length);
+
+			fileContent = "\nnn-nn\n---n-n-\n";
+
+			var ast = grammar.Match (fileContent);
+			if (ast.Success == false) {
+				Trace.WriteLine (ast.ErrorMessage);
+				Dump(ast, res);
+				Trace.WriteLine (res.ToString());
+				Console.ReadLine ();
+				return String.Empty;
+			}
+			var m = ast.Matches ["eol", true].Matches;
+			Dump(ast, res); 
+			Cut(ast, res, SectionType, SectionName);
 			return res.ToString ();
 		}
+
 		public bool IsSubnodeToRemove(Eto.Parse.Match node, string SectionType, string SectionName)
 		{
 			var type = node["sectiontitle",true].StringValue.ToLowerInvariant();
