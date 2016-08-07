6a2e7348df2e17453871ff91d14e82e299df904b
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-08-07 17:19:05 +0300

    unsaved changes

---
+++ mpt-core/PackageProcessing/Package.cs
@@ -0,0 +1,65 @@
+﻿// (c) 2015 Oren Novotny, MIT, https://raw.githubusercontent.com/onovotny/ReferenceGenerator/f17905224655e482f0e57174f83b12852b6043c0/LICENSE.txt
+using System;
+
+namespace mptcore
+{
+	public class Package : IEquatable<Package>, IComparable<Package>
+	{
+		public Package(string id, string version)
+		{
+			Id = id;
+			Version = SemanticVersion.Parse(version, null);
+			VersionString = version;
+		}
+
+		public string Id
+		{
+			get;
+			private set;
+		}
+
+		// This needs a SemVer comparer for sorting
+		public SemanticVersion Version
+		{
+			get;
+			private set;
+		}
+
+		// we use the string for round-tripping
+		public string VersionString
+		{
+			get; private set;
+		}
+
+
+		public int CompareTo(Package other)
+		{
+			// sort on name for now
+			return StringComparer.OrdinalIgnoreCase.Compare(Id, other?.Id);
+		}
+
+		public bool Equals(Package other)
+		{
+			if (ReferenceEquals(other, null))
+				return false;
+
+			return string.Equals(Id, other.Id, StringComparison.OrdinalIgnoreCase) &&
+				Equals(Version, other.Version);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Package);
+		}
+
+		public override int GetHashCode()
+		{
+			return unchecked(Id?.GetHashCode() ?? 1 ^ Version?.GetHashCode() ?? 1);
+		}
+
+		public override string ToString()
+		{
+			return Id + ", " + VersionString;
+		}
+	}
+}
---
+++ mpt-core/PackageProcessing/Reference.cs
@@ -0,0 +1,50 @@
+﻿// (c) 2015 Oren Novotny, MIT, https://raw.githubusercontent.com/onovotny/ReferenceGenerator/f17905224655e482f0e57174f83b12852b6043c0/LICENSE.txt
+using System;
+
+namespace mptcore
+{
+	class Reference : IEquatable<Reference>
+	{
+		public Reference(string name, Version version)
+		{
+			Name = name;
+			Version = version;
+		}
+
+		public string Name
+		{
+			get;
+			private set;
+		}
+
+		public Version Version
+		{
+			get;
+			private set;
+		}
+
+		public bool Equals(Reference other)
+		{
+			if (ReferenceEquals(other, null))
+				return false;
+
+			return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase) &&
+				Equals(Version, other.Version);
+		}
+
+		public override bool Equals(object obj)
+		{
+			return Equals(obj as Reference);
+		}
+
+		public override int GetHashCode()
+		{
+			return unchecked(Name?.GetHashCode() ?? 1 ^ Version?.GetHashCode() ?? 1);
+		}
+
+		public override string ToString()
+		{
+			return Name + ", " + Version;
+		}
+	}
+}
