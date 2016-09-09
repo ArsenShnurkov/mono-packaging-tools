8519b6e667fcbc80409096a44ed3d22782767942
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-09-09 11:09:18 +0300

    Processing for files list was added

--- mpt-core/sln/ProjectTools.cs
+++ mpt-core/sln/ProjectTools.cs
@@ -1,26 +1,51 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
-using CWDev.SLNTools.Core;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using System.Xml;
 
-public class SolutionTools
+public class ProjectTools
 {
-	public static void RemoveProject(string solutionFullPath, string projectName)
+	private const string namespaceName = "http://schemas.microsoft.com/developer/msbuild/2003";
+	public static void DumpFiles(string projectFilename, string baseDirectory)
 	{
-		Console.WriteLine($"Removing {projectName} from {solutionFullPath}");
-		var listToRemove = new List<Project>();
-		var sln = SolutionFile.FromFile(solutionFullPath);
-		var projList = sln.Projects;
-		foreach (var p in projList)
+		List<string> files = new List<string>();
+		var stream = new MemoryStream(File.ReadAllBytes(projectFilename)); // cache file in memoty
+		var document = XDocument.Load(stream);
+		var xmlNamespaceManager = new XmlNamespaceManager(new NameTable());
+		xmlNamespaceManager.AddNamespace("ns", namespaceName);
+		IEnumerable<XElement> listOfSourceFiles = document.XPathSelectElements("/ns:Project/ns:ItemGroup/ns:Compile[@Include]", xmlNamespaceManager);
+		foreach (var el in listOfSourceFiles)
 		{
-			if (string.Compare(p.ProjectName, projectName) == 0)
+			files.Add(el.Attribute("Include").Value);
+		}
+		IEnumerable<XElement> listOfContentFiles = document.XPathSelectElements("/ns:Project/ns:ItemGroup/ns:Content[@Include]", xmlNamespaceManager);
+		foreach (var el in listOfContentFiles)
+		{
+			files.Add(el.Attribute("Include").Value);
+		}
+		string separator = new string(Path.DirectorySeparatorChar, 1);
+		string projDir = (new FileInfo(projectFilename)).Directory.FullName;
+		foreach (string relativeName in files)
+		{
+			var correctedRelativeName = relativeName.Replace("\\", separator);
+			string fullFileName = Path.Combine(projDir, correctedRelativeName);
+			fullFileName = new FileInfo(fullFileName).FullName;
+			string shortName;
+			if (fullFileName.StartsWith(baseDirectory))
 			{
-				listToRemove.Add(p);
+				shortName = fullFileName.Substring(baseDirectory.Length);
+				if (shortName.StartsWith(separator))
+				{
+					shortName = shortName.Substring(1);
+				}
 			}
+			else
+			{
+				shortName = fullFileName;
+			}
+			Console.WriteLine(shortName);
 		}
-		foreach (var r in listToRemove)
-		{
-			sln.Projects.Remove(r);
-		}
-		sln.Save(); // or .SaveAs("NewName.sln");
 	}
 }
