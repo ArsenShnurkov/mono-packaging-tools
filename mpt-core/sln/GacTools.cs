77a9a477980743d8ec7d8337668d57542b385fad
ArsenShnurkov <Arsen.Shnurkov@gmail.com>
2016-11-17 18:57:02 +0300

    list of assemblies from gac

---
+++ mpt-core/sln/GacTools.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace mptcore
+{
+	public class GacTools
+	{
+		SortedList<string, string> assemblies = new SortedList<string, string>();
+
+		public GacTools()
+		{
+			BuildListOfAssemblies();
+		}
+
+		/// http://stackoverflow.com/questions/1599575/enumerating-assemblies-in-gac
+		/// http://stackoverflow.com/a/1557981/6017919
+		void BuildListOfAssemblies()
+		{
+			string path = "/usr/lib/mono/gac";
+
+			if (Directory.Exists(path))
+			{
+				string[] assemblyFolders = Directory.GetDirectories(path);
+				foreach (string assemblyFolder in assemblyFolders)
+				{
+					assemblies.Add(assemblyFolder.Substring(path.Length + 1).ToLower(), assemblyFolder);
+				}
+			}
+		}
+		public bool IsAssemblyInGAC(string assemblyName)
+		{
+			if (assemblies.ContainsKey(assemblyName.ToLower()))
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
+
